96fcd62b7e76e35fcb99182c8cf53a76fdbb3f6b
satyam-sm <satyam101905@gmail.com>
2019-10-19 19:08:20 +0530

    adding tree code

--- src/tree/Tree.java
+++ src/tree/Tree.java
@@ -10,14 +10,34 @@ public class Tree {
 		this.left = null;
 		this.right = null;
 	}
-	
+
 	protected void printInOrder(Tree root) {
-		if(root== null) {
-			return ;
+		if (root == null) {
+			return;
 		}
-		
+
 		printInOrder(root.left);
 		System.out.print(root.data + " ");
 		printInOrder(root.right);
+	}
+
+	protected void preOrder(Tree root) {
+		if (root == null) {
+			return;
+		}
+		System.out.print(root.data + " ");
+
+		preOrder(root.left);
+		preOrder(root.right);
+	}
+
+	protected void postOrder(Tree root) {
+		if (root == null) {
+			return;
+		}
+
+		preOrder(root.left);
+		preOrder(root.right);
+		System.out.print(root.data + " ");
 	}
 }
