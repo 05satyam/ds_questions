afac933ebad61b012a0dd6bbd702947969cd2222
satyam-sm <satyam101905@gmail.com>
2019-12-10 23:23:00 +0530

    unique permutation

---
+++ src/mix_questions/PossibleUniquePermutation.java
@@ -0,0 +1,49 @@
+/*
+ * Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+Example:
+
+Input: [1,1,2]
+Output:
+[
+  [1,1,2],
+  [1,2,1],
+  [2,1,1]
+]
+ * 
+ * */
+
+
+package mix_questions;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PossibleUniquePermutation {
+	public static ArrayList<ArrayList<Integer>> permuteUnique(int[] num) {
+		ArrayList<ArrayList<Integer>> returnList = new ArrayList<ArrayList<Integer>>();
+		returnList.add(new ArrayList<Integer>());
+	 
+		for (int i = 0; i < num.length; i++) {
+			Set<ArrayList<Integer>> currentSet = new HashSet<ArrayList<Integer>>();
+			for (List<Integer> l : returnList) {
+				for (int j = 0; j < l.size() + 1; j++) {
+					l.add(j, num[i]);
+					ArrayList<Integer> T = new ArrayList<Integer>(l);
+					l.remove(j);
+					currentSet.add(T);
+				}
+			}
+			returnList = new ArrayList<ArrayList<Integer>>(currentSet);
+		}
+	 
+		return returnList;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
