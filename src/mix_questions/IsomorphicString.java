a7993d81f47c668f8e78622d53a13364f1b334c0
satyam-sm <satyam101905@gmail.com>
2019-12-11 22:35:50 +0530

    isomorphic string

---
+++ src/mix_questions/IsomorphicString.java
@@ -0,0 +1,42 @@
+package mix_questions;
+
+import java.util.Arrays;
+
+public class IsomorphicString {
+	static int size = 256; 
+    static boolean areIsomorphic(String str1, String str2) 
+    { 
+        int m = str1.length(); 
+        int n = str2.length(); 
+        if(m != n) 
+            return false; 
+        Boolean[] marked = new Boolean[size]; 
+        Arrays.fill(marked, Boolean.FALSE); 
+        int[] map = new int[size]; 
+        Arrays.fill(map, -1); 
+          
+        for (int i = 0; i < n; i++) 
+        { 
+            if (map[str1.charAt(i)] == -1) 
+            { 
+                if (marked[str2.charAt(i)] == true) 
+                    return false; 
+                marked[str2.charAt(i)] = true; 
+                map[str1.charAt(i)] = str2.charAt(i); 
+            } 
+  
+            else if (map[str1.charAt(i)] != str2.charAt(i)) 
+            return false; 
+        } 
+  
+        return true; 
+    } 
+    public static void main (String[] args)  
+    { 
+        boolean res = areIsomorphic("aab", "xxy"); 
+        System.out.println(res); 
+      
+        res = areIsomorphic("aab", "xyz"); 
+        System.out.println(res); 
+    } 
+}
