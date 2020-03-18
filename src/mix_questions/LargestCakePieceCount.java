3d7f380168d0d75ae46b35a95bfdc213a3540b4f
satyam-sm <satyam101905@gmail.com>
2020-03-19 01:01:52 +0530

    largest cake piece

--- src/mix_questions/LargestCakePieceCount.java
+++ src/mix_questions/LargestCakePieceCount.java
@@ -7,61 +7,64 @@
  * 
  * **/
 
-
 package mix_questions;
 
 import java.util.Scanner;
 
 public class LargestCakePieceCount {
 
-	public int ROW, COL; 
-    public int len=0;
-    static int rowNbr[] = {-1, 1, 0, 0}; 
-    static int colNbr[] = {0, 0, 1, -1};
-    private int countPieceCake(int arr[][]) {
-    	int count=0;
-    	boolean visited [][] =  new boolean[ROW][COL];
-    	for(int i=0;i<ROW;i++) 
-    		for(int j=0;j<COL;j++){
-    			visited[i][j]=true;
-    			recurseCalc(i,j,visited, arr);
-    		count=count>len?count:len;
-    		len=0;
-    	}
+	public int ROW, COL;
+	public int len = 0;
+	static int rowNbr[] = { -1, 1, 0, 0 };
+	static int colNbr[] = { 0, 0, 1, -1 };
+
+	private int countPieceCake(int arr[][]) {
+		int count = 0;
+		boolean visited[][] = new boolean[ROW][COL];
+		for (int i = 0; i < ROW; i++)
+			for (int j = 0; j < COL; j++) {
+				if (isPossibleMove(i, j, visited, arr)) {
+					visited[i][j] = true;
+					recurseCalc(i, j, visited, arr);
+					count = count > len ? count : len;
+					len = 0;
+				}
+			}
 		return count;
-    	
-    }
-    
-    private void recurseCalc(int i,int j, boolean visited[][],int arr[][]) {
-    	
-    	for(int ii=0;ii<4;ii++) {
-    		if(isPossibleMove(i+rowNbr[ii],j+colNbr[ii],visited,arr)) {
-    			len++;
-    			recurseCalc(i+rowNbr[ii],j+colNbr[ii],visited,arr);
-    		}
-    			
-    	}
-    }
-    private boolean isPossibleMove(int i, int j,boolean visited[][], int[][] arr) {
-    	if(i<0 || i>=ROW || j<0 || j>=COL|| visited[i][j] || arr[i][j] == '0' ){
-            return false;
-        }
-        else
-            return true; 
-    }
+
+	}
+
+	private void recurseCalc(int i, int j, boolean visited[][], int arr[][]) {
+
+		for (int ii = 0; ii < 4; ii++) {
+			if (isPossibleMove(i + rowNbr[ii], j + colNbr[ii], visited, arr)) {
+				len++;
+				recurseCalc(i + rowNbr[ii], j + colNbr[ii], visited, arr);
+			}
+
+		}
+	}
+
+	private boolean isPossibleMove(int i, int j, boolean visited[][], int[][] arr) {
+		if (i < 0 || i >= ROW || j < 0 || j >= COL || visited[i][j] || arr[i][j] == '0') {
+			return false;
+		} else
+			return true;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LargestCakePieceCount lpc = new  LargestCakePieceCount();
+		LargestCakePieceCount lpc = new LargestCakePieceCount();
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int arr[][] = new int[n][n];
-		for(int i=0;i<n;i++)
-			for(int j=0;j<n;j++)
-				arr[i][j]= sc.nextInt();
-		lpc.ROW=n;
-		lpc.COL=n;
-		System.out.println(lpc.countPieceCake(arr)-1);
-		
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				arr[i][j] = sc.nextInt();
+		lpc.ROW = n;
+		lpc.COL = n;
+		System.out.println(lpc.countPieceCake(arr) - 1);
+
 	}
 
 }
