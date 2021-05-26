7a2a65bcbd7b328d775b5291cc5224c1a744b4b5
satyam-sm <satyam101905@gmail.com>
2021-05-25 22:55:27 -0700

    sum of digits in bake k

---
+++ src/leetcode/SumOfDigitsKBase.java
@@ -0,0 +1,30 @@
+/*
+Given an integer n (in base 10) and a base k, return the sum of the digits of n after converting n from base 10 to base k.
+After converting, each digit should be interpreted as a base 10 number, and the sum should be returned in base 10.
+
+Example 1:
+
+Input: n = 34, k = 6
+Output: 9
+Explanation: 34 (base 10) expressed in base 6 is 54. 5 + 4 = 9.
+* */
+package leetcode;
+
+public class SumOfDigitsKBase {
+    public static int sumBase(int n, int k) {
+
+        return sumBaseCalc(n/k, k,n%k);
+    }
+    public static int sumBaseCalc(int n, int k, int reminder){
+        if(n<k){
+            return n+reminder;
+        }
+
+        int sumDig = reminder+(n%k);
+        return sumBaseCalc(n/k, k, sumDig);
+
+    }
+    public static void main(String[] a){
+        System.out.println(sumBase(10, 10));
+    }
+}
