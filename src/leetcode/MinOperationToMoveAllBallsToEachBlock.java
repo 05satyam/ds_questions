387d32f28bce01ed0cac1b71d5dd1b9e487088b7
satyam-sm <satyam101905@gmail.com>
2021-05-28 00:28:40 -0700

    Solution 2

--- src/leetcode/MinOperationToMoveAllBallsToEachBlock.java
+++ src/leetcode/MinOperationToMoveAllBallsToEachBlock.java
@@ -40,6 +40,27 @@ public class MinOperationToMoveAllBallsToEachBlock {
         }
          return res;
     }
+
+ //Solution 2 O(n+n)
+ public static int[] minOperations2(String boxes) {
+     int[] ans = new int[boxes.length()];
+     int cnt = 0;
+     int val = 0;
+     for(int i = 0; i < boxes.length(); i++) {
+         val += cnt;
+         ans[i] += val;
+         if(boxes.charAt(i) == '1') cnt++;
+     }
+     cnt = 0;
+     val = 0;
+     for(int i = boxes.length() - 1; i >= 0; i--) {
+         val += cnt;
+         ans[i] += val;
+         if(boxes.charAt(i) == '1') cnt++;
+     }
+     return ans;
+ }
+
     public static void main(String[] a){
         int arr[] = minOperations("001011");
         for (int i=0;i<arr.length;i++)
