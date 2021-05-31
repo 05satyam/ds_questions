87a8128a226787c0f0c1fb57a160226de094e677
satyam-sm <satyam101905@gmail.com>
2021-05-30 22:21:42 -0700

    commenst

--- src/leetcode/CountNumberOfTeams.java
+++ src/leetcode/CountNumberOfTeams.java
@@ -1,3 +1,20 @@
+/*
+* There are n soldiers standing in a line. Each soldier is assigned a unique rating value.
+
+You have to form a team of 3 soldiers amongst them under the following rules:
+
+Choose 3 soldiers with index (i, j, k) with rating (rating[i], rating[j], rating[k]).
+A team is valid if: (rating[i] < rating[j] < rating[k]) or (rating[i] > rating[j] > rating[k]) where (0 <= i < j < k < n).
+Return the number of teams you can form given the conditions. (soldiers can be part of multiple teams).
+
+
+
+Example 1:
+
+Input: rating = [2,5,3,4,1]
+Output: 3
+Explanation: We can form three teams given the conditions. (2,3,4), (5,4,1), (5,3,1).
+* */
 package leetcode;
 
 public class CountNumberOfTeams {
