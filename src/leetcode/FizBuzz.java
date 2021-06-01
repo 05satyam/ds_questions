7b4d06aa0bddf40c97c349263add837f4eec0d83
satyam-sm <satyam101905@gmail.com>
2021-05-31 23:07:22 -0700

    FIZZBUZZ

--- src/leetcode/FizBuzz.java
+++ src/leetcode/FizBuzz.java
@@ -1,3 +1,18 @@
+/*
+* Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i if non of the above conditions are true.
+
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+
+* */
 package leetcode;
 
 import java.util.ArrayList;
