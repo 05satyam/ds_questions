dee76d1408c674e69f68a7ad39a7e35a5971949d
satyam-sm <satyam101905@gmail.com>
2021-06-07 23:43:55 -0700

    CheckIfArrayIsSortedAndRotated

--- src/leetcode/CheckIfArrayIsSortedAndRotated.java
+++ src/leetcode/CheckIfArrayIsSortedAndRotated.java
@@ -1,11 +1,23 @@
 package leetcode;
 
 public class CheckIfArrayIsSortedAndRotated {
-    public boolean check(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-
+    public static boolean check(int[] nums) {
+        int irregularities = 0;
+        int length = nums.length;
+        for (int i=0; i<length; i++) {
+            if (nums[i] > nums[(i + 1) % length])
+                irregularities += 1;
         }
-
-        return false;
+        return irregularities > 1 ? false : true;
+    }
+    public static void main(String[] a){
+        int arr[] = {'2','1','4','5'};
+        int arr1[] = {'2','1'};
+        int arr2[] = {'1','2','3'};
+        int arr3[] = {'3','4','5', '1', '2'};
+        System.out.println(check((arr)));
+        System.out.println(check((arr1)));
+        System.out.println(check((arr2)));
+        System.out.println(check((arr3)));
     }
 }
