e68aa676aec4d3dbf8559db8cd3c60d2e322eedd
satyam-sm <satyam101905@gmail.com>
2021-06-09 22:43:16 -0700

    number of different integer in string : question summary

--- src/leetcode/NumberOfDifferentIntegersInString.java
+++ src/leetcode/NumberOfDifferentIntegersInString.java
@@ -1,3 +1,19 @@
+/*
+You are given a string word that consists of digits and lowercase English letters.
+
+You will replace every non-digit character with a space. For example, "a123bc34d8ef34" will become " 123  34 8  34". Notice that you are left with some integers that are separated by at least one space: "123", "34", "8", and "34".
+
+Return the number of different integers after performing the replacement operations on word.
+
+Two integers are considered different if their decimal representations without any leading zeros are different.
+ Input: word = "a123bc34d8ef34"
+Output: 3
+Explanation: The three different integers are "123", "34", and "8". Notice that "34" is only counted once.
+Example 2:
+
+Input: word = "leet1234code234"
+Output: 2
+ */
 package leetcode;
 
 import java.util.HashSet;
