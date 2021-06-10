7b9f7d310ee89ef4159d41ab5e9d2354a17723c9
satyam-sm <satyam101905@gmail.com>
2021-06-09 22:58:20 -0700

    perfect number: question sumaray

--- src/leetcode/PerfectNumber.java
+++ src/leetcode/PerfectNumber.java
@@ -1,3 +1,18 @@
+/**
+ * A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself. A divisor of an integer x is an integer that can divide x evenly.
+ *
+ * Given an integer n, return true if n is a perfect number, otherwise return false.
+ *
+ *
+ * Input: num = 28
+ * Output: true
+ * Explanation: 28 = 1 + 2 + 4 + 7 + 14
+ * 1, 2, 4, 7, and 14 are all divisors of 28.
+ * Example 2:
+ *
+ * Input: num = 6
+ * Output: true
+ */
 package leetcode;
 
 public class PerfectNumber {
