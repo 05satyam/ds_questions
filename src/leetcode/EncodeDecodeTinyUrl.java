5964bff64cba4e8fe6c315c72f604cb3fedbf2a1
satyam-sm <satyam101905@gmail.com>
2021-06-11 23:56:05 -0700

    encode decode tiny url

---
+++ src/leetcode/EncodeDecodeTinyUrl.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EncodeDecodeTinyUrl {
+    public String encode(String longUrl) {
+        char[] urlChar = longUrl.toCharArray();
+        Map<Integer, String> urlToIdxMap = new HashMap<>();
+        int maxIdx = urlToIdxMap.
+    }
+
+    public String decode(String shortUrl) {
+
+    }
+}
