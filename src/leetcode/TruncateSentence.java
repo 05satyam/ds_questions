5824603bf3c02864ecd33f68cd0c11b9ccf35ab4
satyam-sm <satyam101905@gmail.com>
2021-06-13 22:27:45 -0700

    truncate sentence

---
+++ src/leetcode/TruncateSentence.java
@@ -0,0 +1,27 @@
+/**
+ * A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+ *
+ * For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+ * You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+ *
+ * Input: s = "Hello how are you Contestant", k = 4
+ * Output: "Hello how are you"
+ * Explanation:
+ * The words in s are ["Hello", "how" "are", "you", "Contestant"].
+ * The first 4 words are ["Hello", "how", "are", "you"].
+ * Hence, you should return "Hello how are you".
+ */
+package leetcode;
+
+public class TruncateSentence {
+    public String truncateSentence(String s, int k) {
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<k;i++){
+            sb.append(words[i]) ;
+            if(i<k-1)
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+}
