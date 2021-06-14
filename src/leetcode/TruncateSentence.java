eae1d3e9b0286be8c255fc91cde7ad4174a8b198
satyam-sm <satyam101905@gmail.com>
2021-06-13 22:29:27 -0700

    truncate sentence || adding example

--- src/leetcode/TruncateSentence.java
+++ src/leetcode/TruncateSentence.java
@@ -10,6 +10,13 @@
  * The words in s are ["Hello", "how" "are", "you", "Contestant"].
  * The first 4 words are ["Hello", "how", "are", "you"].
  * Hence, you should return "Hello how are you".
+ *
+ * Input: s = "What is the solution to this problem", k = 4
+ * Output: "What is the solution"
+ * Explanation:
+ * The words in s are ["What", "is" "the", "solution", "to", "this", "problem"].
+ * The first 4 words are ["What", "is", "the", "solution"].
+ * Hence, you should return "What is the solution".
  */
 package leetcode;
 
