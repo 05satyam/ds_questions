073ac76e55a361b2b8d6282a234834cab8422ce9
satyam-sm <satyam101905@gmail.com>
2021-06-14 23:21:54 -0700

    pancake sorting

---
+++ src/leetcode/PancakeSorting.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PancakeSorting {
+    public List<Integer> pancakeSort(int[] arr) {
+        List<Integer> res = new ArrayList<>();
+    }
+}
