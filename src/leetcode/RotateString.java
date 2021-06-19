5d6cdf11b035b1e0933480acc683515c90d2419a
satyam-sm <satyam101905@gmail.com>
2021-06-19 14:43:47 -0700

    roatate string

---
+++ src/leetcode/RotateString.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class RotateString {
+    public boolean rotateString(String s, String goal) {
+        if(s.equalsIgnoreCase(goal))
+            return true;
+        for(int i=0;i<s.length();i++){
+            if(getRoatateString(s, i).equalsIgnoreCase(goal))
+                return true;
+        }
+        return false;
+    }
+    public String getRoatateString(String s, int i){
+        return  s.substring(i)+ s.substring(0,i);
+    }
+}
