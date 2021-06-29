8fdb2d62956cdab434291a0989a84c4ecd226054
satyam-sm <satyam101905@gmail.com>
2021-06-28 23:16:04 -0700

    Implement strStr()

---
+++ src/leetcode/ImplementStrSTr.java
@@ -0,0 +1,28 @@
+/**
+ * Implement strStr().
+ *
+ * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+ *
+ * Clarification:
+ *
+ * What should we return when needle is an empty string? This is a great question to ask during an interview.
+ *
+ * For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: haystack = "hello", needle = "ll"
+ * Output: 2
+ */
+package leetcode;
+
+public class ImplementStrSTr {
+    public int strStr(String haystack, String needle) {
+        if(haystack.contains(needle)){
+            return haystack.indexOf(needle);
+        }
+        return -1;
+    }
+}
