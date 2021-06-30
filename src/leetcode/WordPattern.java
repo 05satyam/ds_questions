65983766e7e93119a1032f984ea6ef8482232ebb
satyam-sm <satyam101905@gmail.com>
2021-06-29 23:09:19 -0700

    pattern match || question

--- src/leetcode/WordPattern.java
+++ src/leetcode/WordPattern.java
@@ -1,4 +1,19 @@
-
+/**
+ * Given a pattern and a string s, find if s follows the same pattern.
+ *
+ * Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: pattern = "abba", s = "dog cat cat dog"
+ * Output: true
+ * Example 2:
+ *
+ * Input: pattern = "abba", s = "dog cat cat fish"
+ * Output: false
+ */
 package leetcode;
 
 import java.util.HashMap;
