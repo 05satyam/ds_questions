390cb17581cec5dcde4d784facf52a801468e5a9
satyam-sm <satyam101905@gmail.com>
2021-06-30 23:47:53 -0700

    binary path Solution2

--- src/leetcode/BinaryTreePath.java
+++ src/leetcode/BinaryTreePath.java
@@ -14,4 +14,28 @@ public class BinaryTreePath {
         if (root.left != null) searchBT(root.left, path + root.val + "->", answer);
         if (root.right != null) searchBT(root.right, path + root.val + "->", answer);
     }
+
+//solution2
+    public List<String> binaryTreePaths2(TreeNode root) {
+        List<String> res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        helper(res, root, sb);
+        return res;
+    }
+
+    private void helper(List<String> res, TreeNode root, StringBuilder sb) {
+        if(root == null) {
+            return;
+        }
+        int len = sb.length();
+        sb.append(root.val);
+        if(root.left == null && root.right == null) {
+            res.add(sb.toString());
+        } else {
+            sb.append("->");
+            helper(res, root.left, sb);
+            helper(res, root.right, sb);
+        }
+        sb.setLength(len);
+    }
 }
