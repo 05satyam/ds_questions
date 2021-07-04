7c0615bef41f9bc181aa17cad90550d4e077d9f9
satyam-sm <satyam101905@gmail.com>
2021-07-03 23:09:45 -0700

    SetMismatch

---
+++ src/leetcode/SetMismatch.java
@@ -0,0 +1,29 @@
+/**
+ * ou have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+ *
+ * You are given an integer array nums representing the data status of this set after the error.
+ *
+ * Find the number that occurs twice and the number that is missing and return them in the form of an array.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,2,2,4]
+ * Output: [2,3]
+ */
+package leetcode;
+
+public class SetMismatch {
+    public int[] findErrorNums(int[] nums) {
+        int[] res = new int[2];
+        for (int i : nums) {
+            if (nums[Math.abs(i) - 1] < 0) res[0] = Math.abs(i);
+            else nums[Math.abs(i) - 1] *= -1;
+        }
+        for (int i=0;i<nums.length;i++) {
+            if (nums[i] > 0) res[1] = i+1;
+        }
+        return res;
+    }
+}
