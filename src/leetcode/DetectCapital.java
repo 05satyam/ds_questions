4c78d9f023d2c02d28489022153cee0ffb9a1c37
satyam-sm <satyam101905@gmail.com>
2021-07-09 22:47:44 -0700

    capital count solution2

--- src/leetcode/DetectCapital.java
+++ src/leetcode/DetectCapital.java
@@ -38,4 +38,20 @@ public class DetectCapital {
         }
         return true;
     }
+
+
+    //SOLUTION 2
+    public boolean detectCapitalUse2(String word) {
+        char[] arr = word.toCharArray();
+        int count = 0;
+
+        for(int i= 0; i<word.length(); i++) {
+            if(Character.isUpperCase(arr[i])){
+                count ++;
+            }
+        }
+
+        if(count == 0 || count == word.length()) return true;
+        return count == 1 && Character.isUpperCase(arr[0]);
+    }
 }
