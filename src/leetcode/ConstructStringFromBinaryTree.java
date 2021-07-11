14fb0cd0a4684e6838fc209cd98960a30fd76000
satyam-sm <satyam101905@gmail.com>
2021-07-10 22:23:08 -0700

    string tree

--- src/leetcode/ConstructStringFromBinaryTree.java
+++ src/leetcode/ConstructStringFromBinaryTree.java
@@ -1,3 +1,8 @@
+/**
+ * Given the root of a binary tree, construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way, and return it.
+ *
+ * Omit all the empty parenthesis pairs that do not affect the one-to-one mapping relationship between the string and the original binary tree.
+ */
 package leetcode;
 
 public class ConstructStringFromBinaryTree {
