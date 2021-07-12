7a388219b698f30d54d29f7a39fbeede01726466
satyam-sm <satyam101905@gmail.com>
2021-07-11 22:31:13 -0700

    lucky intg

---
+++ src/leetcode/LuckyIntegerInArray.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class LuckyIntegerInArray {
+    public int findLucky(int[] arr) {
+        int[] nums = new int[501];
+        for(int a : arr) nums[a]++;
+        for(int i = 500;i>0;i--)
+            if(nums[i]==i) return i;
+        return -1;
+    }
+}
