578c209204610b90d554dd6eaa10a61f0a6cdcb7
satyam-sm <satyam101905@gmail.com>
2021-07-12 21:55:45 -0700

    lucky intg

---
+++ src/leetcode/SumOfLeftLeaves.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class SumOfLeftLeaves {
+    int sum=0;
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        int sum = 0;
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            sum += root.left.val;
+        } else {
+            sum += sumOfLeftLeaves(root.left);
+        }
+        sum += sumOfLeftLeaves(root.right);
+        return sum;
+    }
+}
