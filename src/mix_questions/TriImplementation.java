7c5ebfd3224bfedefd528ec1afcafffbd8c82374
Satyam Mittal <satyam101905@gmail.com>
2021-07-27 22:11:46 -0700

    tri basic implementation

---
+++ src/mix_questions/TriImplementation.java
@@ -0,0 +1,53 @@
+package mix_questions;
+
+public class TriImplementation {
+    static final int ALPHABET_SIZE = 26;
+    static class TrieNode {
+        TrieNode[] children = new TrieNode[ALPHABET_SIZE];
+
+        // isEndOfWord is true if the node represents
+        // end of a word
+        boolean isEndOfWord;
+
+        TrieNode(){
+            isEndOfWord = false;
+            for (int i = 0; i < ALPHABET_SIZE; i++)
+                children[i] = null;
+        }
+    }
+    static TrieNode root;
+    static void insert(String key)
+    {
+        int level;
+        int length = key.length();
+        int index;
+
+        TrieNode pCrawl = root;
+
+        for (level = 0; level < length; level++)
+        {
+            index = key.charAt(level) - 'a';
+            if (pCrawl.children[index] == null)
+                pCrawl.children[index] = new TrieNode();
+
+            pCrawl = pCrawl.children[index];
+        }
+
+        // mark last node as leaf
+        pCrawl.isEndOfWord = true;
+    }
+
+    public static void main(String[] a){
+        String keys[] = {"the", "a", "there", "answer", "any",
+                "by", "bye", "their"};
+
+        String output[] = {"Not present in trie", "Present in trie"};
+
+
+        root = new TrieNode();
+        int i;
+        for (i = 0; i < keys.length ; i++)
+            insert(keys[i]);
+
+    }
+}
