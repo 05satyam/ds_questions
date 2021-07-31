ce0b47c6d2fb215290d5f70547ef3a3003959a1a
Satyam Mittal <satyam101905@gmail.com>
2021-07-30 22:56:37 -0700

    Fixes

--- src/leetcode/BuildAnArrayFromPermutation.java
+++ src/leetcode/BuildAnArrayFromPermutation.java
@@ -13,6 +13,8 @@
  * ans = [nums[nums[0]], nums[nums[1]], nums[nums[2]], nums[nums[3]], nums[nums[4]], nums[nums[5]]]
  *     = [nums[0], nums[2], nums[1], nums[5], nums[3], nums[4]]
  *     = [0,1,2,4,5,3]
+ *
+ *
  */
 package leetcode;
 
@@ -24,5 +26,17 @@ public class BuildAnArrayFromPermutation {
             res[i] = nums[nums[i]];;
         }
         return res;
+    }
+
+    public static int numberOfVowelsInString(String s){
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a' ||s.charAt(i)=='e' ||s.charAt(i)=='i' ||s.charAt(i)=='o' ||s.charAt(i)=='u')
+                count++;
+        }
+        return count;
+    }
+    public static void main(String[] a){
+        System.out.print(numberOfVowelsInString("This is a strings"));
     }
 }
