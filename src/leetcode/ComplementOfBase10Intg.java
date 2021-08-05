f2415ed84909a97394495a6216804ab17b85d03a
Satyam Mittal <satyam101905@gmail.com>
2021-08-04 22:03:52 -0700

    Complement of Base 10 Integer

--- src/leetcode/ComplementOfBase10Intg.java
+++ src/leetcode/ComplementOfBase10Intg.java
@@ -6,7 +6,6 @@
  * For a given number n in base-10, return the complement of it's binary representation as a base-10 integer.=
  *
  * Example 1:
- *
  * Input: n = 5
  * Output: 2
  * Explanation: 5 is "101" in binary, with complement "010" in binary, which is 2 in base-10.
