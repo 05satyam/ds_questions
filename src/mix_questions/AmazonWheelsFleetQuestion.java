9aafad82529820f275c34fc3c66b11bc59ddbea2
Satyam Mittal <satyam101905@gmail.com>
2021-08-17 23:13:38 -0700

    coin change variety realted to amazon wheels question

--- src/mix_questions/AmazonWheelsFleetQuestion.java
+++ src/mix_questions/AmazonWheelsFleetQuestion.java
@@ -3,6 +3,10 @@ package mix_questions;
 import java.util.ArrayList;
 import java.util.List;
 
+
+// two type of wheels 2 wheeler and 4 wheeler
+
+
 public class AmazonWheelsFleetQuestion {
     public static int countCurrency(int amount)
     {
@@ -12,7 +16,7 @@ public class AmazonWheelsFleetQuestion {
         int[] notes = new int[]{2,4 };
         int[] noteCounter = new int[9];
 
-10
+
         for (int i = 0; i < 2; i++) {
             if (amount >= notes[i]) {
                 noteCounter[i] = amount / notes[i];
@@ -22,7 +26,7 @@ public class AmazonWheelsFleetQuestion {
 
         // Print notes
        int count=0;
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 2; i++) {
             if (noteCounter[i] != 0) {
                 count++;
             }
