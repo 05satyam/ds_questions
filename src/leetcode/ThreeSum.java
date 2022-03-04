657b65d81013ff10204eb50c08f3c15f0dddba9f
Satyam Mittal <satyam101905@gmail.com>
2022-03-03 23:17:41 -0800

    3sum more easy approach

--- src/leetcode/ThreeSum.java
+++ src/leetcode/ThreeSum.java
@@ -104,6 +104,42 @@ public class ThreeSum {
         return res;
     }
 
+
+//more easy approach
+    public List<List<Integer>> threeSum3(int[] nums) {
+
+        Set<List<Integer>> threeSumSet  = new HashSet<>();
+
+        Arrays.sort(nums);
+
+        for(int i=0; i<nums.length-2;i++){                         //Doing length-2, because we want two elems j and k after i.
+            int j =i+1;
+
+            int k = nums.length-1;
+
+            while(j<k){
+
+                int sum = nums[i]+nums[j]+nums[k];
+
+                if(sum==0){
+                    threeSumSet.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    j++;
+                    k--;
+                }
+                else if (sum > 0){
+                    k--;
+                }
+                else if (sum < 0){
+                    j++;
+                }
+            }
+
+        }
+
+        return new ArrayList<>(threeSumSet);
+    }
+
     public static void main(String[] s){
         int arr[] = {-1,0,1,2,-1,-4 };
         System.out.println( "main " + threeSum(arr));
