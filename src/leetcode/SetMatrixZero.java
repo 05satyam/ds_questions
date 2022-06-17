ed8eddf91a8d6b19be80f66eac8a5f27e449ea78
Satyam Mittal <satyam101905@gmail.com>
2022-06-16 19:53:15 -0700

    set matrix zero

--- src/leetcode/SetMatrixZero.java
+++ src/leetcode/SetMatrixZero.java
@@ -1,34 +1,49 @@
+/**
+ * Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+ *
+ * You must do it in place.
+ */
 package leetcode;
 
 public class SetMatrixZero {
     public void setZeroes(int[][] matrix) {
-            for(int i=0;i<matrix.length;i++){
-                for(int j=0;j<matrix.length;j++){
-                    if(matrix[i][j]==-1)
-                        continue;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]==Integer.MAX_VALUE)
+                    continue;
+                //System.out.println("1.  i j "+ i + j);
+                if(matrix[i][j]==0){
 
-                    if(matrix[i][j]==0){
-                        for(int k=0;k<matrix[i].length;k++){
-                            matrix[i][k]=-1;
+                    for(int k=0;k<matrix[i].length;k++){
+                        if(matrix[i][k]!=0 ){
+                            System.out.println("1.  i K "+ i + k);
+                            matrix[i][k]=((-1)*Integer.MAX_VALUE);
                         }
 
-                        for(int k=i;k>=0;k--){
-                            matrix[k][j]=-1;
-                        }
+                    }
 
-                        for(int k=i;k<matrix.length;k++){
-                            matrix[k][j]=-1;
-                        }
+                    for(int k=i;k>=0;k--){
+                        if(matrix[k][j]!=0){
+                            System.out.println("2.  k j "+ k + j);
+                            matrix[k][j]=((-1)*Integer.MAX_VALUE);}
+                    }
+
+                    for(int k=i;k<matrix.length;k++){
+                        if(matrix[k][j]!=0){
+                            System.out.println("3.  k j "+ k + j);
+                            matrix[k][j]=((-1)*Integer.MAX_VALUE);}
                     }
                 }
-            }
 
-            for(int i=0;i<matrix.length;i++){
-                for(int j=0;j<matrix[i].length;j++)
-                    if(matrix[i][j]==-1)
-                        matrix[i][j]=0;
-            }
 
+            }
+        }
+
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++)
+                if(matrix[i][j]==((-1)*Integer.MAX_VALUE))
+                    matrix[i][j]=0;
+        }
     }
 
 }
