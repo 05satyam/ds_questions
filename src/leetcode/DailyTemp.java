02480323340b61031b28f8ef74b344882f3daa0d
Satyam Mittal <satyam101905@gmail.com>
2023-06-06 12:26:17 -0700

    739

--- src/leetcode/DailyTemp.java
+++ src/leetcode/DailyTemp.java
@@ -1,8 +1,6 @@
 /**
  * Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
- *
- *
- *
+
  * Example 1:
  *
  * Input: temperatures = [73,74,75,71,69,72,76,73]
